8bf93179b0220caf3edc649546844135854c9682
stitch <macypa@gmail.com>
2018-12-09 19:26:31 +0200

    function for calculating ratings

--- lib/storeHall/ratings/ratings.ex
+++ lib/storeHall/ratings/ratings.ex
@@ -52,4 +52,20 @@ defmodule StoreHall.Ratings do
     %UserRating{}
     |> UserRating.changeset(attrs)
   end
+
+  def calc_rating(value, count \\ 0, rating \\ 4, c \\ 2)
+  def calc_rating(value, 0, rating, _c), do: calc_rating(value, 1, rating)
+
+  def calc_rating(data, _count, rating, c) when is_list(data) do
+    data
+    |> Enum.reduce(%{count: 0, rating: rating}, fn value, acc ->
+      %{count: acc.count + 1, rating: calc_rating(value, acc.count, acc.rating, c)}
+    end)
+    |> Map.get(:rating)
+  end
+
+  def calc_rating(value, count, rating, c) do
+    multiplier = c / (count + 2)
+    Float.round((value - rating) * multiplier + rating, 2)
+  end
 end
