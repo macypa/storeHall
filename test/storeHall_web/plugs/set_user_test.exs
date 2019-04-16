2907f475fa42c5ba3effcaa3295fb33cb4fd2eb0
stitch <macypa@gmail.com>
2019-04-17 00:47:09 +0300

    test assigning logged user from session

---
+++ test/storeHall_web/plugs/set_user_test.exs
@@ -0,0 +1,38 @@
+defmodule StoreHallWeb.SetUserTest do
+  use StoreHallWeb.ConnCase
+  use ExUnitProperties
+
+  alias StoreHall.Fixture
+
+  test "assign nil as user, guest as token", %{conn: conn} do
+    conn = get(conn, Routes.user_path(conn, :index))
+
+    case conn.private.plug_session["logged_user"] do
+      nil ->
+        assert conn.assigns.logged_user == nil
+        assert conn.assigns.user_token == "guest"
+
+      _ ->
+        assert false
+    end
+  end
+
+  test "assign user and token from session", %{conn: conn} do
+    user = Fixture.generate_user()
+
+    conn =
+      conn
+      |> Plug.Test.init_test_session(%{logged_user: user})
+
+    conn =
+      conn
+      |> get(Routes.user_path(conn, :index))
+
+    assert conn.assigns.logged_user == user
+
+    {:ok, user_id} =
+      Phoenix.Token.verify(conn, "user token", conn.assigns.user_token, max_age: 60)
+
+    assert user_id == user.id
+  end
+end
