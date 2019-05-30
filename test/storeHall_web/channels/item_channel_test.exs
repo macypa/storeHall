4c3faddab33917402a849ebcb89c49a11ea5bf9b
stitch <macypa@gmail.com>
2019-05-30 21:12:17 +0300

    test broadcasting user rating on changing item rating

--- test/storeHall_web/channels/item_channel_test.exs
+++ test/storeHall_web/channels/item_channel_test.exs
@@ -4,6 +4,7 @@ defmodule StoreHallWeb.ItemChannelTest do
 
   alias StoreHall.Fixture
   alias StoreHallWeb.UserSocket
+  alias StoreHallWeb.UsersChannel
   alias StoreHallWeb.ItemsChannel
 
   alias StoreHall.Comments.ItemComment
@@ -36,6 +37,14 @@ defmodule StoreHallWeb.ItemChannelTest do
 
       assert_broadcast "new_rating", %{new_rating: _}
       assert_broadcast "update_rating", %{new_rating: _}
+
+      user_topic = StoreHallWeb.UsersChannel.topic_prefix() <> "/" <> rating["user_id"]
+
+      assert_receive %Phoenix.Socket.Broadcast{
+        event: "update_rating",
+        topic: ^user_topic,
+        payload: %{new_rating: _}
+      }
     end
 
     test "error on duplicate", %{socket: socket, item: item, user: user} do
@@ -55,6 +64,17 @@ defmodule StoreHallWeb.ItemChannelTest do
       push(guest_socket, "reaction:wow", %{"data" => nil})
 
       assert_push("error", %{message: "must be logged in"})
+    end
+
+    @tag :skip
+    test "broadcast updating user rating", %{socket: socket} do
+      push(socket, "reaction:wow", %{"data" => nil})
+
+      assert_receive %Phoenix.Socket.Broadcast{
+        event: "update_rating",
+        topic: "/users/" <> _,
+        payload: %{new_rating: _}
+      }
     end
 
     test "wow", %{socket: socket} do
@@ -101,12 +121,14 @@ defmodule StoreHallWeb.ItemChannelTest do
 
     token = Phoenix.Token.sign(@endpoint, "user token", user.id)
     {:ok, socket} = connect(UserSocket, %{"token" => token})
-    {:ok, _, socket} = subscribe_and_join(socket, ItemsChannel, "/items/#{item.id}")
+    {:ok, _, item_socket} = subscribe_and_join(socket, ItemsChannel, "/items/#{item.id}")
 
     {:ok, guest_socket} = connect(UserSocket, %{"token" => "guest"})
     {:ok, _, guest_socket} = subscribe_and_join(guest_socket, ItemsChannel, "/items/#{item.id}")
 
-    {:ok, socket: socket, guest_socket: guest_socket, item: item, user: user}
+    {:ok, _, _user_socket} = subscribe_and_join(socket, UsersChannel, "/users/" <> user.id)
+
+    {:ok, socket: item_socket, guest_socket: guest_socket, item: item, user: user}
   end
 
   defp dencode(schema) do
