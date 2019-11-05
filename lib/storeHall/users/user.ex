c176c5b9ec762954f17666937302bf7e4f22eb57
stitch <macypa@gmail.com>
2019-11-05 17:05:44 +0200

    item/user css

--- lib/storeHall/users/user.ex
+++ lib/storeHall/users/user.ex
@@ -5,7 +5,7 @@ defmodule StoreHall.Users.User do
 
   @primary_key {:id, :string, []}
   @derive {Phoenix.Param, key: :id}
-  @derive {Jason.Encoder, only: [:id, :email, :first_name, :last_name, :image]}
+  @derive {Jason.Encoder, only: [:id, :email, :first_name, :last_name, :image, :details]}
   schema "users" do
     field :email, :string, unique: true
     field :first_name, :string
