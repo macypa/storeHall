2c5da1d95858ee1ee139932726d866d4db556e27
stitch <macypa@gmail.com>
2019-11-11 18:23:23 +0200

    range slider for price and rating

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,29 +3,34 @@ import './semantic-ui-css/semantic.js';
 
 $(document).ready(function() {
 
-  $('.ui.checkbox')
-    .checkbox()
-  ;
-
-  $('#double').range({
-    min: 0,
-    max: 10,
-    start: 5,
-    step: 1,
-    verbose: true,
-    debug: true,
-    onChange: function(value) {
-      var
-        $self = $(this),
-        firstVal = $self.range('get thumb value'),
-        secVal = $self.range('get thumb value', 'second');
-      $('#display-d').html('|' + firstVal + " - " + secVal + '| = ' + value);
-    }
-  });
-
 });
 
+function getVals(){
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
 
-
+  var displayElement = parent.getElementsByClassName("rangeValues")[0];
+  displayElement.getElementsByClassName("min")[0].innerHTML = slide1;
+  displayElement.getElementsByClassName("max")[0].innerHTML = slide2;
+}
+window.onload = function(){
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName("range-slider");
+  for( var x = 0; x < sliderSections.length; x++ ){
+    var sliders = sliderSections[x].getElementsByTagName("input");
+    for( var y = 0; y < sliders.length; y++ ){
+      if( sliders[y].type ==="range" ){
+        sliders[y].oninput = getVals;
+        // Manually trigger event first time to display values
+        sliders[y].oninput();
+      }
+    }
+  }
+}
 //import * as $ from 'jquery';
 //$("icon").addClass("ui-btn-b ui-shadow ui-corner-all ui-btn-icon-notext ui-btn-inline");
