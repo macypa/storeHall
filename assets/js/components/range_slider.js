958ff47f69f90b0c3d99b0a499c161a415e455db
stitch <macypa@gmail.com>
2019-11-15 08:33:49 +0200

    sliders value field as numeric input

--- assets/js/components/range_slider.js
+++ assets/js/components/range_slider.js
@@ -1,25 +1,39 @@
 
 var sliderSections = $(".range-slider");
 for (let sliderSections of sliderSections) {
+
   var sliders = sliderSections.getElementsByTagName("input");
+  var displayElement = sliderSections.getElementsByClassName("rangeValues")[0];
+  displayElement.getElementsByClassName("min")[0].oninput = setVals;
+  displayElement.getElementsByClassName("max")[0].oninput = setVals;
+
   for( var y = 0; y < sliders.length; y++ ){
     if( sliders[y].type ==="range" ){
+      sliders[y].removeAttribute("name");
       sliders[y].oninput = getVals;
-      // Manually trigger event first time to display values
-      sliders[y].oninput();
     }
   }
+}
+
+function setVals(){
+  var parent = this.parentNode.parentNode;
+  var slides = parent.getElementsByTagName("input");
+  var field_min = slides[0];
+  var field_max = slides[1];
+
+  slides[2].value = field_min.value;
+  slides[3].value = field_max.value;
 }
 
 function getVals(){
   // Get slider values
   var parent = this.parentNode;
   var slides = parent.getElementsByTagName("input");
-  var slide1 = parseFloat( slides[0].value );
-  var slide2 = parseFloat( slides[1].value );
+  var slide1 = parseFloat( slides[2].value );
+  var slide2 = parseFloat( slides[3].value );
 
-  var slides1_name = slides[0].name;
-  var slides2_name = slides[1].name;
+  var slides1_name = slides[2].name;
+  var slides2_name = slides[3].name;
 
   // Neither slider will clip the other, so make sure we determine which is larger
   if ( slide1 > slide2 ) {
@@ -28,17 +42,20 @@ function getVals(){
     slide1 = tmp;
 
     if (slides1_name.includes("min")) {
-      slides[0].name = slides2_name;
-      slides[1].name = slides1_name;
+      slides[2].name = slides2_name;
+      slides[3].name = slides1_name;
     }
   } else {
     if (slides1_name.includes("max")) {
-      slides[0].name = slides2_name;
-      slides[1].name = slides1_name;
+      slides[2].name = slides2_name;
+      slides[3].name = slides1_name;
     }
   }
 
   var displayElement = parent.getElementsByClassName("rangeValues")[0];
-  displayElement.getElementsByClassName("min")[0].innerHTML = slide1;
-  displayElement.getElementsByClassName("max")[0].innerHTML = slide2;
+  displayElement.getElementsByClassName("min")[0].value = slide1;
+  displayElement.getElementsByClassName("max")[0].value = slide2;
+
+  //need to trigger for only one of the inputs
+  $(displayElement.getElementsByClassName("min")[0]).trigger('change');
 }
