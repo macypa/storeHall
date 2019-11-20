fed31ec79d843485857b7550d8355dbf3bc77225
stitch <macypa@gmail.com>
2019-11-20 20:24:51 +0200

    page title

--- lib/storeHall_web/views/layout_view.ex
+++ lib/storeHall_web/views/layout_view.ex
@@ -1,3 +1,4 @@
 defmodule StoreHallWeb.LayoutView do
   use StoreHallWeb, :view
+  import StoreHallWeb.PageTitle
 end
---
+++ lib/storeHall_web/views/page_title.ex
@@ -0,0 +1,24 @@
+defmodule StoreHallWeb.PageTitle do
+  alias StoreHallWeb.{ItemView, UserView}
+  require StoreHallWeb.Gettext
+
+  @suffix "Крали Марко"
+
+  def page_title(assigns), do: assigns |> get
+  def page_title_with_suffix(assigns), do: page_title(assigns) |> put_suffix
+
+  defp put_suffix(nil), do: @suffix
+  defp put_suffix(title), do: title <> " - " <> @suffix
+
+  defp get(%{users: user}), do: StoreHallWeb.Gettext.gettext("Listing users")
+
+  defp get(%{user: user}) do
+    user.first_name <> " " <> user.last_name
+  end
+
+  defp get(%{item: item}) do
+    item.name
+  end
+
+  defp get(_), do: nil
+end
