a0733ad99b29788fd70a7583144c0821f0c7c8a0
stitch <macypa@gmail.com>
2019-12-28 01:31:02 +0200

    func to get city from geolocation

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -87,6 +87,23 @@ window.debounced = function(delay, fn) {
   }
 }
 
+window.get_city = function() {
+  $.ajax({
+		url: "https://geolocation-db.com/jsonp",
+		jsonpCallback: "callback",
+		dataType: "jsonp",
+		success: function( location ) {
+      window.city = location.city;
+			// $('#country').html(location.country_name);
+			// $('#state').html(location.state);
+			// $('#city').html(location.city);
+			// $('#latitude').html(location.latitude);
+			// $('#longitude').html(location.longitude);
+			// $('#ip').html(location.IPv4);
+		}
+	});
+}
+
 $("input").not($(":button")).keypress(function (evt) {
   if (evt.keyCode == 13) {
     let iname = $(this).val();
